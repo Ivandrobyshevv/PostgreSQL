13a9e44013b9333777f5feb8aeb09e5a1cc66e3b
Ivan Drobyshev <drobyshev_ivan00@mail.ru>
2022-10-14 12:22:41 +0500

    first commit

---
+++ Соединения - JOINS.sql
@@ -0,0 +1,26 @@
+--1. Найти заказчиков и обслуживающих их заказы сотрудников таких, что и заказчики и сотрудники из города London, а доставка идёт компанией Speedy Express. Вывести компанию заказчика и ФИО сотрудника.
+SELECT c.company_name, CONCAT(e.first_name, '', e.last_name)
+FROM orders AS o
+JOIN customers AS c USING(customer_id)
+JOIN employees AS e USING(employee_id)
+JOIN shippers AS s ON o.ship_via = s.shipper_id
+WHERE c.city = 'London' AND e.city = 'London' AND s.company_name = 'Speedy Express';
+
+--2. Найти активные (см. поле discontinued) продукты из категории Beverages и Seafood, которых в продаже менее 20 единиц. Вывести наименование продуктов, кол-во единиц в продаже, имя контакта поставщика и его телефонный номер.
+SELECT product_name, units_in_stock, contact_name, phone
+FROM products
+JOIN categories USING(category_id)
+JOIN suppliers USING(supplier_id)
+WHERE category_name IN ('Beverages', 'Seafood') AND units_in_stock < 20 AND discontinued = 0;
+
+--3. Найти заказчиков, не сделавших ни одного заказа. Вывести имя заказчика и order_id.
+SELECT contact_name, order_id
+FROM customers
+LEFT JOIN orders USING(customer_id)
+WHERE order_id IS NULL;
+
+--4. Переписать предыдущий запрос, использовав симметричный вид джойна (подсказка: речь о LEFT и RIGHT).
+SELECT contact_name, order_id
+FROM orders
+RIGHT JOIN customers USING(customer_id)
+WHERE order_id IS NULL;
